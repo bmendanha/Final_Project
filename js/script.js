5993f35008675947cc3d827f37b9edf1aa829e7b
Bruno Mendanha <bmendanha@hotmail.com>
2020-12-09 09:40:55 +0000

    Inserted Animation Script on Home and About section

--- js/script.js
+++ js/script.js
@@ -1,15 +1,49 @@
-$(document).ready(function () {
-  $(window).scroll(function () {
-    if (this.scrollY > 20) {
-      $('.navbar').addClass('sticky');
-    } else {
-    }
-    $('.navbar').removeClass('sticky');
+$(document).ready(function(){
+  $(window).scroll(function(){
+      // sticky navbar on scroll script
+      if(this.scrollY > 20){
+          $('.navbar').addClass("sticky");
+      }else{
+          $('.navbar').removeClass("sticky");
+      }
+      
+      // scroll-up button show/hide script
+      if(this.scrollY > 500){
+          $('.scroll-up-btn').addClass("show");
+      }else{
+          $('.scroll-up-btn').removeClass("show");
+      }
   });
+
+  // slide-up script
+  $('.scroll-up-btn').click(function(){
+    $('html').animate({scrollTop: 0});
+    // removing smooth scroll on slide-up button click
+    $('html').css("scrollBehavior", "auto");
+});
+$('.navbar .menu li a').click(function(){
+    // applying again smooth scroll on menu items click
+    $('html').css("scrollBehavior", "smooth");
+});
   //toggle menu/navbar script
   $('.menu-btn').click(function () {
     $('.navbar .menu').toggleClass('active');
     $('.menu-btn i').toggleClass('active');
+  });
+
+  //typing animation script - Home section
+  var typed = new Typed('.typing', {
+    strings: ['Performance', 'Health', 'Body sculpting', 'Strength'],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+  //typing animation script - About Section
+  var typed = new Typed('.typing2', {
+    strings: ['Personal Trainer', 'Sensei', 'Coach', 'Sport Manager'],
+    typeSpeed: 100,
+    backSpeed: 30,
+    loop: true,
   });
   //owl-caroulsel Script
   $('.carousel').owlCarousel({
@@ -33,3 +67,4 @@ $(document).ready(function () {
     },
   });
 });
+
