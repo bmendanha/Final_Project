40ca7e02f68283475049ff6746405a051bb2a30f
Bruno Mendanha <bmendanha@hotmail.com>
2020-12-09 15:44:16 +0000

    Fixed CSS - Home and Navbar

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
-$(document).ready(function(){
-  $(window).scroll(function(){
-      // sticky navbar on scroll script
-      if(this.scrollY > 20){
-          $('.navbar').addClass("sticky");
-      }else{
-          $('.navbar').removeClass("sticky");
-      }
-      
-      // scroll-up button show/hide script
-      if(this.scrollY > 500){
-          $('.scroll-up-btn').addClass("show");
-      }else{
-          $('.scroll-up-btn').removeClass("show");
-      }
+$(document).ready(function () {
+  $(window).scroll(function () {
+    // sticky navbar on scroll script
+    if (this.scrollY > 20) {
+      $('.navbar').addClass('sticky');
+    } else {
+      $('.navbar').removeClass('sticky');
+    }
+
+    // scroll-up button show/hide script
+    if (this.scrollY > 500) {
+      $('.scroll-up-btn').addClass('show');
+    } else {
+      $('.scroll-up-btn').removeClass('show');
+    }
   });
 
   // slide-up script
-  $('.scroll-up-btn').click(function(){
-    $('html').animate({scrollTop: 0});
+  $('.scroll-up-btn').click(function () {
+    $('html').animate({ scrollTop: 0 });
     // removing smooth scroll on slide-up button click
-    $('html').css("scrollBehavior", "auto");
-});
-$('.navbar .menu li a').click(function(){
+    $('html').css('scrollBehavior', 'auto');
+  });
+  $('.navbar .menu li a').click(function () {
     // applying again smooth scroll on menu items click
-    $('html').css("scrollBehavior", "smooth");
-});
+    $('html').css('scrollBehavior', 'smooth');
+  });
   //toggle menu/navbar script
   $('.menu-btn').click(function () {
     $('.navbar .menu').toggleClass('active');
@@ -35,7 +35,7 @@ $('.navbar .menu li a').click(function(){
   var typed = new Typed('.typing', {
     strings: ['Performance', 'Health', 'Body sculpting', 'Strength'],
     typeSpeed: 100,
-    backSpeed: 60,
+    backSpeed: 30,
     loop: true,
   });
   //typing animation script - About Section
@@ -67,4 +67,3 @@ $('.navbar .menu li a').click(function(){
     },
   });
 });
-
