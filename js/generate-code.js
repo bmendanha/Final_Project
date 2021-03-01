96f15f2c7cceb6658ee6e45ff5261537c390ebd4
Bruno Mendanha <bmendanha@hotmail.com>
2021-03-01 19:34:30 +0000

    Generating physical training

--- js/generate-code.js
+++ js/generate-code.js
@@ -2,7 +2,8 @@ function mostracodigo() {
   var frequency = $("input[name='freq']:checked").val();
   var interest = $("input[name='goal']:checked").val();
   var biotype = $("input[name='shape']:checked").val();
-  var codeAssessment = frequency + interest + biotype + '.pdf';
-  alert('Os campos selecionados foram: ' + frequency + interest + biotype);
-  alert('O valor da variavel codigo foi: ' + codeAssessment);
+  var codeAssessment =
+    './assessments/' + frequency + interest + biotype + '.pdf';
+ alert('The variable name: ' + codeAssessment);
+  window.open(codeAssessment);
 }
